0ede27daec377ae2549640c75656d1996a741dc2
Ronan Salvador <ronansalvador@yahoo.com.br>
2022-08-12 12:17:04 -0300

    Requisito 4

--- routes/ProductsRoute.js
+++ routes/ProductsRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productController = require('../controllers/ProductController');
+const validations = require('../middlewares/productValidation');
 
 const route = express.Router();
 
 route.get('/', productController.getAllProducts);
 route.get('/:id', productController.getProduct);
-route.post('/', productController.createProduct);
+route.post('/', validations.isNameFieldValid, productController.createProduct);
 
 module.exports = route;
